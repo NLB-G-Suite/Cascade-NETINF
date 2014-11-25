2c503ca9aa8570722e0f1e1e79dca386a1818fd9
dfcf93 <campod2@rpi.edu>
2014-11-25 14:29:03 -0700

    fixed style stuff

--- netinfredditprep.py
+++ netinfredditprep.py
@@ -194,7 +194,7 @@ def PrintCascades(cascades, filename):
             total += len(chain)
             count +=1
             for i in chain:
-                s += str(username_to_id[i.username.rstrip('\n')]) + "," + str(i.unixtime) + ";"
+                s += str(username_to_id[i.username.rstrip('\n')]) + "," + str(i.unixtime) + ","
             f.write(s[:-1] + "\n")
     print("Average: " + str(total/count))
 if __name__ == '__main__':
