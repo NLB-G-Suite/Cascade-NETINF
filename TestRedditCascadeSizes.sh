903d2709a4c036a6ccf27728430ae19c10bfab89
Ubuntu <cloud@atlasworker.atlasworker.f1.internal.cloudapp.net>
2014-12-04 20:06:12 +0000

    ran netinf on reddit 0-20

--- TestRedditCascadeSizes.sh
+++ TestRedditCascadeSizes.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-COUNTER=0
-rm -rf TestReddit
-mkdir TestReddit
-while [  $COUNTER -lt 2 ]; do
-    rm -rf $COUNTER
-    mkdir $COUNTER
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250' > '250log'$COUNTER'.txt'
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:500 -o:'TestReddit/'$COUNTER'output-500' > '500log'$COUNTER'.txt'
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:100 -o:'TestReddit/'$COUNTER'output-100' > '100log'$COUNTER'.txt'
+COUNTER=4
+while [  $COUNTER -lt 8 ]; do
+    echo Running  $COUNTER with 250
+    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250' > 'TestReddit/250log'$COUNTER'.txt'
+    echo Running  $COUNTER with 500
+    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:500 -o:'TestReddit/'$COUNTER'output-500' > 'TestReddit/500log'$COUNTER'.txt'
+    echo Running  $COUNTER with 100
+    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:100 -o:'TestReddit/'$COUNTER'output-100' > 'TestReddit/100log'$COUNTER'.txt'
     let COUNTER=COUNTER+1 
 done
