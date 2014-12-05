9adcb26781e8feb6fa1057cb38670ea88031538a
dfcf93 <campod2@rpi.edu>
2014-12-05 16:51:12 -0500

    Reorg and added related files

--- TestRedditCascadeSizes.sh
+++ TestRedditCascadeSizes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 COUNTER=1
 while [  $COUNTER -lt 19 ]; do
-    ./netinf -i: $COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250' > 'TestReddit/250log'$COUNTER'.txt'
+    ./netinf -i: $COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250'
 done
