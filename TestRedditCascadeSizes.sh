c17ead032a1b6b493199f80992c6389efe41402b
dfcf93 <campod2@rpi.edu>
2014-12-05 01:16:44 -0500

    rearanged

--- TestRedditCascadeSizes.sh
+++ TestRedditCascadeSizes.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-COUNTER=4
-while [  $COUNTER -lt 8 ]; do
-    echo Running  $COUNTER with 250
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250' > 'TestReddit/250log'$COUNTER'.txt'
-    echo Running  $COUNTER with 500
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:500 -o:'TestReddit/'$COUNTER'output-500' > 'TestReddit/500log'$COUNTER'.txt'
-    echo Running  $COUNTER with 100
-    ./netinf -i:'Scripts&Stuff/Cascades/'$COUNTER'cascades.txt' -e:100 -o:'TestReddit/'$COUNTER'output-100' > 'TestReddit/100log'$COUNTER'.txt'
-    let COUNTER=COUNTER+1 
+COUNTER=1
+while [  $COUNTER -lt 19 ]; do
+    ./netinf -i: $COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'output-250' > 'TestReddit/250log'$COUNTER'.txt'
 done
