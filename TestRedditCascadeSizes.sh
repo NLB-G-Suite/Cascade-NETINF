33e5fe38c7c9b87dbfb485cbd4edbcbf57cfeab8
Ubuntu <cloud@atlasworker.atlasworker.f1.internal.cloudapp.net>
2014-12-07 19:34:48 +0000

    iterations 1-19

--- TestRedditCascadeSizes.sh
+++ TestRedditCascadeSizes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNTER=1
 while [  $COUNTER -lt 19 ]; do
-    ./netinf -i:$COUNTER'cascades.txt' -e:100 -o:'TestReddit/'$COUNTER'-100' &
+    ./netinf -i:$COUNTER'cascades.txt' -e:250 -o:'TestReddit/'$COUNTER'-250' &
     let COUNTER=COUNTER+1 
 done
