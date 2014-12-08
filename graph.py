dafe8dba2d5ce311db268e68466ab0abc5e53093
dfcf93 <campod2@rpi.edu>
2014-12-08 01:07:06 -0500

    psh

--- graph.py
+++
@@ -1,9 +0,0 @@
-import snap
-import sys
-if __name__ == '__main__':
-    filename = sys.argv[1]
-    Graph = snap.LoadEdgeList(snap.PNGraph, filename+".txt", 0, 1)
-    labels = snap.TIntStrH()
-    for NI in Graph.Nodes():
-        labels[NI.GetId()] = str(NI.GetId())
-    snap.DrawGViz(Graph, snap.gvlDot, filename+".png", filename)
